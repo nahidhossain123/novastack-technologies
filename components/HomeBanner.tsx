b4ad62a55e09b1667dd8656a6f1378c4aa8dbbb7
Nahid Hossain <nahidhossain351@gmail.com>
2025-12-07 13:43:16 +0600

    responsive issues

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -8,7 +8,7 @@ const HomeBanner = () => {
     return (
         <div className="h-screen flex flex-wrap md:flex-nowrap justify-end items-center container">
             <div className="w-full text-center md:text-start md:max-w-[450px] xl:max-w-[650px]">
-                <div className="space-y-5 md:space-y-10">
+                <div className="space-y-5 pt-16 md:p-0 md:space-y-10">
                     <h1 className='text-primary-200 text-4xl md:text-4xl xl:text-6xl font-bold'>Trusted Effective Service and Solutions</h1>
                     <h3 className='text-primary-200 text-lg md:text-2xl font-bold'>NovaStack Technologies</h3>
                     <p><a href="/">NovaStack Technologies</a> is one of the oldest and best software solution provider in Bangladesh. NovaStack Technologies provides all kinds of software solutions all over the country. POS Retails software’s, Vat Management, ERP System, Web application development, Apps Development and much more.</p>
