3f9df3ac93938191b92da69bd3177c6efe1dc7ad
Nahid Hossain <nahidhossain351@gmail.com>
2025-12-07 12:05:37 +0600

    feat:add form validation and typescript types

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,7 +1,61 @@
-import React from 'react'
+'use client'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
 import ThemeButton from './ui/ThemeButton'
+import { FormData, FormErrors } from '@/types';
 
 const ContactForm = () => {
+    const [form, setForm] = useState<FormData>({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const [errors, setErrors] = useState<FormErrors>({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    // Handle input changes
+    const handleChange = (
+        e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    // Validate form
+    const validate = (): boolean => {
+        const newErrors: FormErrors = {};
+
+        if (!form.name.trim()) {
+            newErrors.name = "Name is required.";
+        }
+
+        if (!form.email.trim()) {
+            newErrors.email = "Email is required.";
+        } else if (!/^\S+@\S+\.\S+$/.test(form.email)) {
+            newErrors.email = "Invalid email format.";
+        }
+
+        if (!form.message.trim()) {
+            newErrors.message = "Message is required.";
+        }
+
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    };
+
+    // Submit handler
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        if (validate()) {
+            alert("Form submitted successfully!");
+            console.log("Form Data:", form);
+
+            setForm({ name: "", email: "", message: "" });
+        }
+    };
+
     return (
         <section className="py-10 md:py-28 px-3 md:p-0 bg-skyblue-100">
             <div className="container mx-auto space-y-10">
@@ -16,34 +70,46 @@ const ContactForm = () => {
                     </div>
                     <div className="flex justify-center items-center w-full">
                         <form className="w-full bg-white shadow-lg rounded-lg p-8 space-y-6">
-                            <div className="flex flex-col">
+                            <div className='w-full space-y-2'>
                                 <input
                                     type="text"
+                                    name="name"
+                                    value={form.name}
+                                    onChange={handleChange}
                                     placeholder="Your Name"
-                                    className="bg-gray-100 rounded-md px-3 py-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                    className="w-full bg-gray-100 rounded-md px-3 py-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
                                 />
+                                {errors.name && (<p className='text-red-500 text-sm italic ml-3'>{errors.name}</p>)}
                             </div>
 
-                            <div className="flex flex-col">
+                            <div className='w-full space-y-2'>
                                 <input
                                     type="email"
+                                    name="email"
+                                    value={form.email}
+                                    onChange={handleChange}
                                     placeholder="Your Email"
-                                    className="bg-gray-100 rounded-md px-3 py-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                    className="w-full bg-gray-100 rounded-md px-3 py-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
                                 />
+                                {errors.email && (<p className='text-red-500 text-sm italic ml-3'>{errors.email}</p>)}
                             </div>
 
-                            <div className="flex flex-col">
+                            <div className='w-full space-y-2'>
                                 <textarea
                                     placeholder="Your Message"
-                                    className="bg-gray-100 rounded-md px-3 py-4 focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none
+                                    name="message"
+                                    value={form.message}
+                                    onChange={handleChange}
+                                    className="w-full bg-gray-100 rounded-md px-3 py-4 focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none
                                     h-32           
                                     md:h-56 
                                     "
                                 ></textarea>
+                                {errors.message && (<p className='text-red-500 text-sm itali ml-3'>{errors.message}</p>)}
                             </div>
 
                             {/* Submit Button */}
-                            <ThemeButton style="" >
+                            <ThemeButton style="" onButtonClick={handleSubmit} >
                                 Send Message
                             </ThemeButton>
 
